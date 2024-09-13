568580c61cb675f1acfbabd7258ef8d7224428df
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-13 13:04:51 +0400

    add styles to Subnav component

--- app/componets/Subnav/Subnav.tsx
+++ app/componets/Subnav/Subnav.tsx
@@ -3,7 +3,16 @@ import s from "./Subnav.module.css"
 
 function Subnav() {
   return (
-    <div>Subnav</div>
+    <>
+    <div className={s.subnav}>
+        <ul className={s.subnavUl}>
+            <li><h2>რეგიონი</h2></li>
+            <li><h2>საფასო კატეგორია</h2></li>
+            <li><h2>ფართობი</h2></li>
+            <li><h2>საძინებლის რაოდენობა</h2></li>
+        </ul>
+    </div>
+    </>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import s from "./page.module.css"
+import Subnav from './componets/Subnav/Subnav'
 
 function page() {
   return (
     <>
+    <Subnav/>
     </>
   )
 }
