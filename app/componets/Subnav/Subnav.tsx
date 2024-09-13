9ec8ef680a8e9de77bf22e1b04558b88c79243a6
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-13 13:29:13 +0400

    add styles to subnav component

--- app/componets/Subnav/Subnav.tsx
+++ app/componets/Subnav/Subnav.tsx
@@ -4,15 +4,18 @@ import s from "./Subnav.module.css"
 function Subnav() {
   return (
     <>
-    <div className={s.subnav}>
-        <ul className={s.subnavUl}>
-            <li><h2>რეგიონი</h2></li>
-            <li><h2>საფასო კატეგორია</h2></li>
-            <li><h2>ფართობი</h2></li>
-            <li><h2>საძინებლის რაოდენობა</h2></li>
-        </ul>
-        <div className={s.subnav2}>
-            a
+    <div className={s.main}>
+        <div className={s.subnav}>
+            <ul className={s.subnavUl}>
+                <li><h2>რეგიონი</h2></li>
+                <li><h2>საფასო კატეგორია</h2></li>
+                <li><h2>ფართობი</h2></li>
+                <li><h2>საძინებლის რაოდენობა</h2></li>
+            </ul>
+        </div>
+        <div  className={s.test}>
+            <button>+ ლისტინგის დამატება</button>
+            <button>+ აგენტის დამატება</button>
         </div>
     </div>
     </>
