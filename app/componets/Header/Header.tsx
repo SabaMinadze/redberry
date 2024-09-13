edab084dee1d757a67d130be44c855ebdd7ad035
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-13 12:36:20 +0400

    Add header.tsx component

---
+++ app/componets/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from "./Header.module.css"
+
+function Header() {
+  return (
+    <>
+    <div>
+        <h1>REDBERRY</h1>
+    </div>
+    </>
+  )
+}
+
+export default Header
