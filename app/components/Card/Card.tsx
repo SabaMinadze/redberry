da68a0fa2a71ea26ca3999049e051cfd6321e3b6
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-13 15:21:06 +0400

    install font awesome in my project and import location logo add content in card.tsx and add styles

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import s from "./Card.module.css"
 import room from "./room1.png"
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Card() {
   return (
@@ -12,6 +14,11 @@ function Card() {
         <button className={s.isRentBtn}>ქირავდება</button>
       </div>
       <div className={s.cardContent}>
+        <div className={s.cardContent2}>
+          <h1>80 000 ₾</h1>
+          <h2 className={s.location}><FontAwesomeIcon icon={faMapMarkerAlt} style={{ fontSize: '24px' }} /></h2>
+
+        </div>
 
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import s from "./page.module.css"
 import Subnav from './components/Subnav/Subnav'
 import Cards from './components/Cards/Cards'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false; // Prevent Font Awesome from adding its own CSS
 
 function page() {
   return (
