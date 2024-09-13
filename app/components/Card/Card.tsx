a6c14f11b43da7782426b532e3eb8d842a091fb3
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-13 18:39:20 +0400

    Update Card.tsx

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBed } from '@fortawesome/free-solid-svg-icons'; // Use the free solid icon
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { faChartArea } from '@fortawesome/free-solid-svg-icons';
-import { faHandPeace } from '@fortawesome/free-solid-svg-icons';
 import { faSignsPost } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -22,14 +21,12 @@ function Card() {
       <div className={s.cardContent}>
           <div className={s.cardContent2}>
             <h1>80 000 ₾</h1>
-              <div className={s.cardErrorBox}>
-                <h3 className={s.location}><FontAwesomeIcon icon={faMapMarkerAlt} style={{ fontSize: '20px' }} /> თბილისი, ი, ჭავჭავაძის 53</h3>
-                <div className={s.iconBox}>
-                <h3 className={s.inconStyle}><FontAwesomeIcon icon={faBed} className={s.location}/> 2</h3>
-                <h3 className={s.inconStyle}><FontAwesomeIcon icon={faChartArea} className={s.location}/> 55მ</h3>
-                <h3 className={s.inconStyle}><FontAwesomeIcon icon={faSignsPost} className={s.location}/> 0160</h3>
-              </div>
-            </div>
+            <h3 className={s.location}><FontAwesomeIcon icon={faMapMarkerAlt} style={{ fontSize: '20px' }} /> თბილისი, ი, ჭავჭავაძის 53</h3>
+            <div className={s.iconBox}>
+            <h3 className={s.inconStyle}><FontAwesomeIcon icon={faBed} className={s.location}/> 2</h3>
+            <h3 className={s.inconStyle}><FontAwesomeIcon icon={faChartArea} className={s.location}/> 55მ</h3>
+            <h3 className={s.inconStyle}><FontAwesomeIcon icon={faSignsPost} className={s.location}/> 0160</h3>
+          </div>
         </div>
       </div>
     </div>
