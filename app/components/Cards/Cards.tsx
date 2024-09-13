23af9475b58d0fe357c29707ff25a48d9579e8c8
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-13 19:06:00 +0400

    Here I test data.json file i i sole this errors by adding correct path where image url coming

--- app/components/Cards/Cards.tsx
+++ app/components/Cards/Cards.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import s from "./Cards.module.css"
+import data from "./data.json"
+import Image from 'next/image'
 import Card from '../Card/Card'
 
 function Cards() {
   return (
     <>
     <div className={s.mainCards}>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div>{data.map(info =>
+        <>
+       <h3>{info.id}</h3>
+       <h3>{info.address}</h3>
+       <h3>{info.price}</h3>
+       <Image src={info.image} alt={'error'} width={100} height={100}/>
+       </>
+    )}</div>
+    <Card/>
     </div>
-    <div className={s.mainCards}>
-        <Card/>
-        <Card/>
-        <Card/>
-    </div>
+ 
 
     </>
   )
