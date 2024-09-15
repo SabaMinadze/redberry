4862936bfab8d52d1578d55da01aa265a6b7c5d3
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-15 17:52:12 +0400

    create new add route from adding new house content

---
+++ app/add/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from "./page.module.css"
+
+function page() {
+  return (
+    <>
+    <div>
+        <h1>ლისტინგის დამატება</h1>
+    </div>
+    </>
+  )
+}
+
+export default page
